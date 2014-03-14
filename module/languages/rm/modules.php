511f690ae340370dfb17a7f3fb5e7f77903a0380
Avibot <avibot@avisota.org>
2014-03-14 04:00:26 +0100

    Auto-Update translations from transifex

---
+++ module/languages/rm/modules.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/avisota-contao/language/rm/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-03-11T16:08:29+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['MOD']['avisota-history']['0'] = 'Avisota - Cronologia';
+$GLOBALS['TL_LANG']['MOD']['avisota-history']['1'] = 'Cronologia e statistica simpla per messadis dad Avisota.';
+
